0ff5c17f629be07e7425a72388193668ae628b65
chbfiv <chbfiv@gmail.com>
2015-01-23 21:08:36 -0800

    imported art some basic scripts

---
+++ Assets/Scripts/ControlTest.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ControlTest : MonoBehaviour {
+
+	public GameObject target;
+	public Vector3 rot;
+	public Material skybox;
+	public float exposure;
+
+	public void HandleMinusClick() {
+		if (target != null) {
+			target.transform.Rotate(-1f*rot);
+		}
+		if (skybox != null) {
+			float currentExposure = skybox.GetFloat("_Exposure");
+			skybox.SetFloat("_Exposure", currentExposure + (-1f*exposure));
+		}
+	}
+	
+	public void HandlePlusClick() {
+		if (target != null) {
+			target.transform.Rotate(rot);
+		}
+
+		if (skybox != null) {
+			float currentExposure = skybox.GetFloat("_Exposure");
+			skybox.SetFloat("_Exposure", currentExposure + exposure);
+		}
+	}
+}
---
+++ Assets/Scripts/Editor/MazeGenerator.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+using System;
+
+public class MazeGenerator : EditorWindow {
+
+	public float step = 4f;
+	public GameObject root;
+
+	[MenuItem ("GlobalGameJam2015/Maze Generator %0")]
+	public static void ShowMazeGenerator() {
+		MazeGenerator window = (MazeGenerator)EditorWindow.GetWindow (typeof (MazeGenerator));
+	}
+	
+	private void OnGUI () {
+		GUILayout.Label ("Maze Generator", EditorStyles.boldLabel);
+		step = EditorGUILayout.FloatField ("Step", step);
+		root = (GameObject)EditorGUILayout.ObjectField ("Root", root, typeof(GameObject), true);
+
+		if (GUILayout.Button("Clear")) {
+			Clean();
+    	}
+
+		if (GUILayout.Button("Build")) {
+			Build();
+		}
+	}
+
+	private void Clean() {
+		
+		if (root == null)
+			throw new InvalidOperationException ("Root not selected");
+
+		GameObject.DestroyImmediate (root);
+	}
+
+	private void Build() {
+
+		if (root == null)
+			throw new InvalidOperationException ("Root not selected");
+
+		GameObject go = new GameObject ();
+		go.name = "test";
+
+		go.transform.parent = root.transform;
+	}
+}
