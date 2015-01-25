f5928b55cdf36280e74f4dc9afde9dccc3ed3e3a
chbfiv <chbfiv@gmail.com>
2015-01-24 23:53:58 -0800

    added AI

---
+++ Assets/Scripts/PlayerAI.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using Pathfinding;
+
+public class PlayerAI : MonoBehaviour {
+
+	public Transform target;
+
+	private Seeker _seeker;
+
+	// Use this for initialization
+	void Start () {
+		_seeker = GetComponent<Seeker> ();
+		_seeker.StartPath (transform.position, target.position, OnPathCompelte);
+	}
+	
+	private void OnPathCompelte(Path p) {
+		Debug.Log ("Nice!");
+	}
+}
